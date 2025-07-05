da97ae929d218031fb1812aae1de610962a1aa1f
Jason Mund <jason.mund@gmx.de>
2025-07-05 22:44:35 +0200

    fix bild

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  basePath: '/api/v1/namespaces/tournamentfox/services/http:tournamentfox-frontend:80/proxy',
+  assetPrefix: '/api/v1/namespaces/tournamentfox/services/http:tournamentfox-frontend:80/proxy',
+  trailingSlash: true,
 };
 
 const withNextIntl = createNextIntlPlugin();
